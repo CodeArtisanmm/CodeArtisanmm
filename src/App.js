ac7ebcfc9a208b7ef6b79101dbca51059974fa87
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2024-01-20 22:43:28 +0630

    new ga commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import AppRouters from './routes/AppRouters';
-import ReactGA from 'react-ga';
 import "./assets/fonts/exo/exo.css";
 import "./assets/fonts/fira-sans/fira-sans.css";
 import "slick-carousel/slick/slick.css"; 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/fontawesome.css";
 import "./assets/scss/style.scss";
-
-ReactGA.initialize('G-9G17G3K81N');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
