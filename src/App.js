a98519de3fedd2731985e31d2165028e829a5a89
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2024-01-20 22:38:58 +0630

    google analytics package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import AppRouters from './routes/AppRouters';
-
+import ReactGA from 'react-ga';
 import "./assets/fonts/exo/exo.css";
 import "./assets/fonts/fira-sans/fira-sans.css";
 import "slick-carousel/slick/slick.css"; 
@@ -9,6 +9,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/fontawesome.css";
 import "./assets/scss/style.scss";
 
+ReactGA.initialize('G-9G17G3K81N');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
