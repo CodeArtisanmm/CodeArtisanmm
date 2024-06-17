e57b5c11fcee6185809789e89997160e8508b4c0
Newstein123 <minthetpaing376@gmail.com>
2024-06-18 06:15:11 +0700

    about us

--- src/containers/Abouts/AboutOne.jsx
+++ src/containers/Abouts/AboutOne.jsx
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { useTransition } from "react";
 import {
   Button,
   ImageBoxOne,
   SecTitle,
   SecSubTitle,
-  InfoMedia
-} from '../../components';
+  InfoMedia,
+} from "../../components";
+import { useTranslation } from "react-i18next";
 
-const AboutOne = ({ className }) => (
-  <div className={`position-relative ${className || ''}`}>
-    <div className="about-shape1 d-none d-xl-block">CodeArtisan</div>
-    <div className="container z-index-common">
-      <div className="row gx-60">
-        <div className="col-lg-6 col-xl-5 mb-50 mb-lg-0">
-          <ImageBoxOne imageOne="images/about/about-1.jpg"
-            imageTwo="images/about/about-2.jpg"
-            video="https://www.dropbox.com/s/b4k0p5swok5l8t2/videoplayback.mp4?dl=1"
-          />
-        </div>
-        <div className="col-lg-6 col-xl-7 align-self-center">
-          <SecSubTitle><i className="fas fa-bring-forward" />Get best It solution 2022</SecSubTitle>
-          <SecTitle className="h1">Choosing the right business partner is Crucial!!!</SecTitle>
-          <p className="mb-4 mt-1 pb-3">Welcome to our digital playground, Code Artisan where creativity meets cutting-edge technology. With a passion for creating stunning websites and a relentless pursuit of technological excellence, we're here to transform your online presence. Join us on this exciting journey, where the fusion of creativity and technology knows no bounds.
-          </p>
-          <InfoMedia
-            image="images/icon/tel-1-1.png"
-            title="24 HOURS SERVICE AVAILABLE"
-            info="Call Us: +959766036922
-            "
-            className="call-media"
-          />
-          <Button path="/about">About Us<i className="far fa-long-arrow-right" /></Button>
+const AboutOne = ({ className }) => {
+  const { t } = useTranslation();
+  return (
+    <div className={`position-relative ${className || ""}`}>
+      <div className="about-shape1 d-none d-xl-block">CodeArtisan</div>
+      <div className="container z-index-common">
+        <div className="row gx-60">
+          <div className="col-lg-6 col-xl-5 mb-50 mb-lg-0">
+            <ImageBoxOne
+              imageOne="images/about/about-1.jpg"
+              imageTwo="images/about/about-2.jpg"
+              video="video/about-us.mp4"
+            />
+          </div>
+          <div className="col-lg-6 col-xl-7 align-self-center">
+            <SecSubTitle>
+              <i className="fas fa-bring-forward" />
+              Get best It solution 2024
+            </SecSubTitle>
+            <SecTitle className="h1">{t("about.title-home")}</SecTitle>
+            <p className="mb-4 mt-1 pb-3">{t("about.first-para")}</p>
+            <InfoMedia
+              image="images/icon/tel-1-1.png"
+              title={t("about.24hours-service")}
+              info={`${t("about.contact-us-on-viber")} +959 770 139 207`}
+              className="call-media"
+            />
+            <Button path="/about">
+              {t("about-us")}
+              <i className="far fa-long-arrow-right" />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AboutOne;
