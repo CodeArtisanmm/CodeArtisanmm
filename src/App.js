a6c9b9494ccf8c73147fbc3c43bf7db0e5a2b044
Phyo-Phyo-Kyaw <phyophyokyawppk1@gmail.com>
2025-03-25 20:55:28 +0630

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouters from "./routes/AppRouters";
 import "./assets/fonts/exo/exo.css";
@@ -9,7 +9,16 @@ import "./assets/css/fontawesome.css";
 import "./assets/scss/style.scss";
 import { I18nextProvider, useTranslation } from "react-i18next";
 
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-HHL4EXVSYJ";
+
+ReactGA.initialize(TRACKING_ID, { debug_mode: true });
+
 function App() {
+  useEffect(() => {
+    ReactGA.send("pageview");
+  }, []);
   const { t, i18n } = useTranslation();
   return (
     <BrowserRouter>
