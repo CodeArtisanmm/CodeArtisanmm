986071ba0460f6c87b0a807217a910272e39f078
minthetawaken <minthet@epik-solutions.com>
2025-03-25 22:17:14 +0700

    Google Analytics Code Change

--- src/App.js
+++ src/App.js
@@ -9,16 +9,7 @@ import "./assets/css/fontawesome.css";
 import "./assets/scss/style.scss";
 import { I18nextProvider, useTranslation } from "react-i18next";
 
-import ReactGA from "react-ga4";
-
-const TRACKING_ID = "G-HHL4EXVSYJ";
-
-ReactGA.initialize(TRACKING_ID, { debug_mode: true });
-
 function App() {
-  useEffect(() => {
-    ReactGA.send("pageview");
-  }, []);
   const { t, i18n } = useTranslation();
   return (
     <BrowserRouter>
